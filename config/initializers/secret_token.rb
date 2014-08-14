1fa9b3dd45320358e130b6dc18b31c6eb5d88370
Nur Rozalina <nur.rozalina@gmail.com>
2014-08-14 17:10:39 +0800

    scaffold status, devise, devise-views, signup-firstname-last-profilename, relational database for status by adding userid via rails g migration add_user_id_to_statuses and editing db-migrate-xxxx_AddUserIdToStatuses page

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Treebook1::Application.config.secret_token = 'a7fe961798aca8ba085ba440b7b6c49cecab362026d33739ef53cae3ff86e1dc05c8077f33ed347e55425db4c30f1cf35bb93a5b16ebe108e2c163da090b05dc'
